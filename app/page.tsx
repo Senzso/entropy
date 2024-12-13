5030d0d3eb320d5e9f4e793e577ebb970dccc197
Senzso <110294817+Senzso@users.noreply.github.com>
2024-12-13 22:27:33 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,20 @@ import { useState } from 'react'
 import Background from '../components/background'
 import Terminal from '../components/terminal'
 import EnterButton from '../components/enter-button'
+import CountdownTimer from '../components/countdown-timer'
 
 export default function Home() {
   const [showTerminal, setShowTerminal] = useState(false)
 
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-4 relative">
-      <div className="flex flex-col items-center space-y-12">
-        <Background fadeOut={showTerminal} />
+      <Background fadeOut={showTerminal} />
+      <div className="flex flex-col items-center space-y-12 z-10">
+        <CountdownTimer />
         {!showTerminal && <EnterButton onClick={() => setShowTerminal(true)} />}
       </div>
       {showTerminal && <Terminal onClose={() => setShowTerminal(false)} />}
     </main>
   )
 }
-
 
