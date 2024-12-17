13d106e2a2f85935766ef9902b1d603ce2a3bcdd
Senzso <110294817+Senzso@users.noreply.github.com>
2024-12-17 23:19:20 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,19 @@ import Background from '../components/background'
 import Terminal from '../components/terminal'
 import EnterButton from '../components/enter-button'
 import CountdownTimer from '../components/countdown-timer'
+import IconRefresher from '../components/IconRefresher'
+import CopyableText from '../components/copyable-text'
 
 export default function Home() {
   const [showTerminal, setShowTerminal] = useState(false)
 
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-4 relative">
+      <IconRefresher />
       <Background fadeOut={showTerminal} />
+      <div className="absolute top-8 left-1/2 transform -translate-x-1/2 z-20">
+        <CopyableText text="TBA" />
+      </div>
       <div className="flex flex-col items-center space-y-8 z-10">
         <CountdownTimer />
         {!showTerminal && <EnterButton onClick={() => setShowTerminal(true)} />}
