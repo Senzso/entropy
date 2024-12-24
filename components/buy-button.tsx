33d28f6ed622eff76e927dcc12e7ba13659d34bc
Senzso <110294817+Senzso@users.noreply.github.com>
2024-12-24 17:00:45 +0100

    Update buy-button.tsx

--- components/buy-button.tsx
+++ components/buy-button.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function BuyButton() {
   return (
     <Link
-      href="https://pump.fun/coin/jSBEjGTL4vYiCgJRYxDTcwCx58EWuF2UcVb2SKhpump"
+      href="https://pump.fun/coin/"
       target="_blank"
       rel="noopener noreferrer"
       className="bg-white text-black px-8 py-3 rounded-full font-bold text-lg transition-transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
