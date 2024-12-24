ff6d7260d755b83d0b31fcad17358b980f1281a9
Senzso <110294817+Senzso@users.noreply.github.com>
2024-12-24 02:12:07 +0100

    Update countdown-timer.tsx

--- components/countdown-timer.tsx
+++ components/countdown-timer.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react'
 
-const TARGET_DATE = new Date('2024-12-24T02:00:00+01:00').getTime()
+const TARGET_DATE = new Date('2024-12-30T02:00:00+01:00').getTime()
 
 interface TimeLeft {
   days: number
